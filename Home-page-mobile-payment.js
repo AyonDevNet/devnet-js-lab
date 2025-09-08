8d6d7aa5e60c5715a3685556caa78e6497764ea4
AyonDevNet <rmcoder7@gmail.com>
2025-09-08 12:49:31 +0600

    add

--- Home-page-mobile-payment.js
+++ Home-page-mobile-payment.js
@@ -1,6 +1,3 @@
-
-
-
 document.getElementById("addMoney").addEventListener('click' , function(event){
 
             event.preventDefault();
@@ -55,7 +52,42 @@ document.getElementById("addMoney").addEventListener('click' , function(event){
 
 
 
-})
+});
+// add money section wrap page section
+
+
+document.getElementById("card").addEventListener("click", function() {
+    const content = document.getElementById("moneyForm");
+        content.classList.toggle("hidden"); // show
+  });
+
+  // cashout section wrap page section
+document.getElementById("cashout-section").addEventListener("click", function() {
+    const CashOutContent = document.getElementById("CashoutForm");
+
+    CashOutContent.classList.toggle("hidden");
+
+
+  });
+
+
+
+// transfer section in here  wrap page section
+
+document.getElementById("transferCard").addEventListener('click', function(){
+
+
+ const tranferTaka = document.getElementById('Transfer-Money')
+
+ tranferTaka.classList.toggle('hidden');
+
+
+});
+
+
+
+
+
 
 
 
@@ -74,31 +106,101 @@ document.getElementById("log-out-panel").addEventListener('click' , function (){
 })
 
 
-// add money section wrap page section
-
-
-document.getElementById("card").addEventListener("click", function() {
-    const content = document.getElementById("moneyForm");
-        content.classList.toggle("hidden"); // show
-  });
-
-  // cashout section wrap page section
-document.getElementById("cashout-section").addEventListener("click", function() {
-    const CashOutContent = document.getElementById("CashoutForm");
-
-    CashOutContent.classList.toggle("hidden");
-  });
 
 
 
-// transfer section in here  wrap page section
-
-document.getElementById("transferCard").addEventListener('click', function(){
 
 
- const tranferTaka = document.getElementById('Transfer-Money')
 
- tranferTaka.classList.toggle('hidden');
+// cashout button
+
+document.getElementById("cashoutBtn").addEventListener('click' , function(){
+
+ const PinAddress = document.getElementById('inputPassword').value
+            
+            // console.log(EmailAddress,PinAddress)
+
+
+            // password verification 
+
+            const pinAlphabet = /^[0-9]+$/;
+              
+
+            if (!pinAlphabet.test(PinAddress)){
+         
+               alert('Make sure you are putting righ input')
+
+            }
+        
+            else {
+
+      // upper digit number
+
+      const mainBalance = document.getElementById("AddBalance").innerText
+
+      const cashSend = document.getElementById('cashOutMoney').value
+
+      const parMainBalance = parseFloat(mainBalance)
+      const parCashsend = parseFloat(cashSend)
+
+
+      const totalCashout = parMainBalance - parCashsend
+
+             const mainBalance3 = document.getElementById("AddBalance").innerText = totalCashout
+             console.log(mainBalance3)
+
+            }
 
 
 });
+
+
+
+
+// transfer money to taka 
+
+document.getElementById("transferTaka").addEventListener('click' , function(){
+
+const PinAddress = document.getElementById('inputPassword').value
+            
+            // console.log(EmailAddress,PinAddress)
+
+
+            // password verification 
+
+            const pinAlphabet = /^[0-9]+$/;
+              
+
+            if (!pinAlphabet.test(PinAddress)){
+         
+               alert('Make sure you are putting righ input')
+
+            }
+        
+            else {
+
+
+
+// upper digit number
+
+      const mainBalance4 = document.getElementById("AddBalance").innerText
+     const cashamountsended = document.getElementById('tranferAmount').value
+
+     const parsAmountTransfer = parseFloat(mainBalance4)
+     const parsAmounsended = parseFloat(cashamountsended)
+
+     
+      const totaltransferAmount = parsAmountTransfer - parsAmounsended
+
+             const mainBalance5 = document.getElementById("AddBalance").innerText = totaltransferAmount
+             console.log(mainBalance5)
+
+     alert("sUCCESSFULLY TRANSFERED")
+
+
+
+            }
+
+
+
+})
